8c0953c58370112716ad6a3a9f21519147166e21
Josip Maric <joza.m44@gmail.com>
2018-03-14 17:23:39 +0100

    my page version 1.0

---
+++ fend-animal-trading-cards-master/Crno 1.js
@@ -0,0 +1,12 @@
+var SamoZaJosipa=true;
+function Ilija()
+{
+    if(SamoZaJosipa===true){
+    var Dj= "Ilija narucio pjesmu Crno i Zlatno";
+	
+    return Dj;}
+    else{
+        console.log("Pusiona");
+    }
+}
+console.log(Ilija());
